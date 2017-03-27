6945f68b8e15598b4c3c963873238388db14fda4
Thomas Thiebaud <thiebaud.tom@gmail.com>
2017-03-27 16:42:22 +0200

    feat(form): Add redux form

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,7 +1,11 @@
 import combineReducers from 'redux/lib/combineReducers'
+import {
+  reducer as formReducer,
+} from 'redux-form'
 
 export default (asyncReducers) => {
   return combineReducers({
     ...asyncReducers,
+    form: formReducer,
   })
 }
