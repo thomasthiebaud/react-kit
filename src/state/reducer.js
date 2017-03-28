0be30dbcdef124fb6a29f2ae38a6da8e500342c4
Thomas Thiebaud <thiebaud.tom@gmail.com>
2017-03-28 08:40:35 +0200

    revert(redux-form): Remove redux-form (to big)

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,11 +1,7 @@
 import combineReducers from 'redux/lib/combineReducers'
-import {
-  reducer as formReducer,
-} from 'redux-form'
 
 export default (asyncReducers) => {
   return combineReducers({
     ...asyncReducers,
-    form: formReducer,
   })
 }
