ffe437aa9c1530da68c846fc90e90e40ac7b86ad
Thomas Thiebaud <thiebaud.tom@gmail.com>
2018-02-09 08:06:20 +0100

    Fix hot reload of sagas and reducers

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,20 +1,9 @@
 import React from 'react'
 import Loadable from 'react-loadable'
 
-import state from '../../state'
-
-import reducer from './reducer'
-import sagas from './sagas'
-
 export default Loadable({
   loader: () => import(/* webpackChunkName: "home" */ './Container'),
   loading() {
     return <div>Loading...</div>
-  },
-  render(Component, props) {
-    state.injectReducer('home', reducer)
-    state.injectSagas(sagas)
-
-    return <Component.default {...props} />
   },
 })
