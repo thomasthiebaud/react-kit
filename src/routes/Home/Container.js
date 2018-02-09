ca242f8740607fa7765778b2ac9b0e6e3c6a932a
Thomas Thiebaud <thiebaud.tom@gmail.com>
2018-02-09 08:06:20 +0100

    Fix hot reload of sagas and reducers

--- src/routes/Home/Container.js
+++ src/routes/Home/Container.js
@@ -1,21 +1,20 @@
 import { connect } from 'react-redux'
 
 import Home from './Home'
+import state from '../../state'
 import reducer from './reducer'
 import sagas from './sagas'
 import { computeGreetings } from './selectors'
+
+state.injectReducer('home', reducer)
+state.injectSagas(sagas)
 
 const mapDispatchToProps = dispatch => ({
   getGreetings: () => dispatch({ type: 'GET_GREETINGS_REQUESTED' }),
 })
 
-const mapStateToProps = state => ({
-  greetings: computeGreetings(state),
+const mapStateToProps = s => ({
+  greetings: computeGreetings(s),
 })
-
-export {
-  reducer,
-  sagas,
-}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
